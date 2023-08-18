16c0cc93069d3bcddc7004cea8cb55778d3ddf40
Rajib_p <silicon.slice@gmail.com>
2023-08-18 23:14:59 +0600

    pass & pass valid confrm: update1

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -12,7 +12,10 @@ export default function Signup() {
   // const [emailAlert, setEmailAlert] = useState(null);
   // const [passwordAlert, setPasswordAlert] = useState(null);
 
+  const [pass, setPass] = useState("");
   const [pass_error, setPassError] = useState("");
+  const [email_error, setEmailError] = useState("");
+  const [pass_confrm_error, setPassConrmError] = useState("");
 
     const loginCredentials = () => {
         // if (loginData.email === 'john@gmail.com' && loginData.password === 'christina') {
@@ -27,26 +30,47 @@ export default function Signup() {
       const { name, value } = ev.target;
       var lowerCaseLetters = /[a-z]/g;
       var upperCaseLetters = /[A-Z]/g;
+      var numbers = /[0-9]/g;
+      let rgxSpclChrtr = /^(?=.*[-\#\$\.\_\~\%\&\@\!\+\=\\*])/;
       // const newData = Object.assign({}, loginData, { [name]: value })
 
       // Set warning/s for password validation
       let passwordError = ''
       if(ev.target.id === 'password'){
         //const { name, value } = ev.target
-        newData = value;
-        if (newData.length < 7) {
-          passwordError = 'Password should be at least 7 characters'
+        // newData = value;
+        setPass(value);
+        if (value.length < 7 || value.length > 12) {
+          passwordError = 'Password should be 7 to 12 characters'
         }
-        if(!newData.match(lowerCaseLetters)) {  
-          passwordError = 'Must include 1 lowercase'
+        if(!value.match(rgxSpclChrtr)) {  
+          passwordError = 'Must include 1 special character';
         }
-        if(!newData.match(upperCaseLetters)) {  
-          passwordError = 'Must include 1 Uppercase'
+        if(!value.match(numbers)) {  
+          passwordError = 'Must include 1 Number';
         }
-        if(newData.length < 1){
-          setPassError("");          
-        } else{
+        if(!value.match(lowerCaseLetters)) {  
+          passwordError = 'Must include 1 lowercase';
+        }
+        if(!value.match(upperCaseLetters)) {  
+          passwordError = 'Must include 1 Uppercase';
+        }
+
+        console.log(value + ' ' + value.length);
+        if(value){
           setPassError(passwordError);          
+        } else{
+          setPassError('');
+        }
+
+      }
+
+      if(ev.target.id === 'passwordconrm'){
+        // newData = value;
+        if(pass === value){
+          setPassConrmError('no-err');
+        } else{
+          setPassConrmError('yes-err');
         }
       }
 
@@ -68,9 +92,11 @@ export default function Signup() {
           </h1>
           <input type="text" placeholder='Full Name' />
           <input type="email" placeholder='Email Address' />
+          <span className="show-clint-err">{email_error}</span>
           <input type="password" name='password' id="password" onChange={handleChange} placeholder='Password' />
           <span className="show-clint-err">{pass_error}</span>
-          <input type="password" placeholder='Password Confirmation' />
+          <input type="password" name='passwordconrm' id="passwordconrm" onChange={handleChange} placeholder='Password Confirmation' />
+          <span className={`show-clint-err ${pass_confrm_error}`}>{pass_confrm_error}</span>
           <button type="submit" className="btn btn-block">Sign up</button>
           <p className="message">
             Already Registered? <Link to="/login">Sign in</Link>
