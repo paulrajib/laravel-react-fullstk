06493f55ea737498fea334c419f9d912d5728d1b
Rajib_p <silicon.slice@gmail.com>
2023-08-18 18:13:48 +0600

    signup form design

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
+
+  const onSignupSubmit = (ev) => {
+    ev.preventdefault();
+  }
+
   return (
-    <div>Signup</div>
+      <div className="login-signup-form animated fadeInDown">
+      <div className="form">
+        <form action="" onSubmit={onSignupSubmit}>
+          <h1 className='title'>
+            Signup for free
+          </h1>
+          <input type="text" placeholder='Full Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+          <input type="password" placeholder='Password Confirmation' />
+          <button className="btn btn-block">Sign up</button>
+          <p className="message">
+            Already Registered? <Link to="/login">Sign in</Link>
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
